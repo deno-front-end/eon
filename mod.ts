0acd864a107a867ed2f6729cff7e411621cb403d
SRNV <alula.rudy@gmail.com>
2020-10-29 18:17:31 +0100

    feat(wip): start getting the dom graph

--- mod.ts
+++ mod.ts
@@ -3,4 +3,5 @@ import { ModuleGetter } from './src/classes/ModuleGetter.ts';
 const component = await ModuleGetter.buildModule({
   entrypoint: './examples/hello-app/HelloApp.tsx',
 });
+
 console.warn(component);
